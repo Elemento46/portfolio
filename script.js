7c545768b6c22911e0d0f6b9c7b15149e309bb9f
Alexandre Rodrigues <alexandre.fastrun@outlook.com>
2025-10-22 23:05:16 -0300

    att portfolio final

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const sr = ScrollReveal({ distance: '50px', duration: 800, easing: 'ease', reset: false, mobile: true, viewFactor: 0.15, viewOffset: { top: 0, right: 0, bottom: 24, left: 0 } });
+const sr = ScrollReveal({
+    distance: '50px',
+    duration: 800,
+    easing: 'ease',
+    reset: false,
+    mobile: true,
+    viewFactor: 0.15,
+    viewOffset: { top: 0, right: 0, bottom: 24, left: 0 }
+});
 sr.reveal('.informações', { origin: 'top', interval: 80 });
 sr.reveal('.sobre', { origin: 'left' });
 sr.reveal('.projetos', { origin: 'right' });
@@ -6,10 +14,10 @@ sr.reveal('.skills', { origin: 'bottom' });
 
 particlesJS('particles-js', {
     particles: {
-        number: { value: 650, density: { enable: true, value_area: 1100 } },
+        number: { value: 850, density: { enable: true, value_area: 900 } },
         color: { value: '#ffffff' },
-        shape: { type: 'triangle' },
-        opacity: { value: 0.18, random: true },
+        shape: { type: 'circle' },
+        opacity: { value: 0.55, random: true },
         size: { value: 3, random: true },
         line_linked: { enable: true, distance: 140, color: '#ffffff', opacity: 0.25, width: 1 },
         move: { enable: true, speed: 2.5, direction: 'none', random: false, straight: false, out_mode: 'out', bounce: false }
